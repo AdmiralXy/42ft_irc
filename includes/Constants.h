042b315782211d28c950875f6d0e00b011c3b493
AdmiralXy <gameproblem80@gmail.com>
2022-05-17 17:11:39 +0300

    remove unused defines

--- includes/Constants.h
+++ includes/Constants.h
@@ -8,11 +8,3 @@
 #define ERROR_SOCKET_SET "Unable to set socket!"
 #define ERROR_IP_BIND "Unable to bind IP or specified port!"
 #define ERROR_SOCKET_LISTEN "Unable to start listening on socket!"
-
-#define ERR_PASSINCORRECT "Password incorrect"
-#define ERR_NEEDMOREPARAMS "Need more parameters"
-#define ERR_ALREADYREGISTRED "Already registered"
-#define ERR_NONICKNAMEGIVEN "No nickname given"
-#define ERR_NICKCOLLISION "Nickname is already in use"
-#define ERR_NOACCESS "You have no rights"
-#define SUCCESS_REGISTER "Hi, welcome to 42ft_irc server!"
